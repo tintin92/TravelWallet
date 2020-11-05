6a4a4c903efbaa0237783e6ed865ad0515b8eb3f
Tintin <tanner@schenck.xyz>
2020-11-04 22:45:53 -0500

    remove unwanted app.js file from staticFilestoPreCache

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,6 @@ const iconFiles = iconSizes.map(
 
 const staticFilesToPreCache = [
   "/",
-  "/app.js",
-  "/favicon.ico",
   "/manifest.webmanifest",
   // "/assets/images/icons/icon-512x512.png",
   // "/assets/images/icons/icon-192x192.png",
